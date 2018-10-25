241cd57c7226f2bea4ef1f70cde13421bfcca4de
fortissimo <43173533+jionniu2hu@users.noreply.github.com>
2018-10-25 12:47:54 +0800

    5152

--- TK10.23/js/myGame.js
+++ TK10.23/js/myGame.js
@@ -14,12 +14,81 @@ var guessCount = 1;
 var resetButton;
 guessField.focus();
 
-function checkGuess(a, b) {
+function checkGuess() {
+    var userGuess = Number(guessField.value);
+    console.log(typeof userGuess);
+    console.log(userGuess);
     //document.write("及第三的坏")
-    guesses.innerHTML += guessField.value + "  ";
+
+    if (guessCount === 1) {
+        guesses.textContent = '上次猜测的数字:'
+    }
+    guesses.textContent += userGuess + " ";
     guesses.style.backgroundColor = "red";
+
+    lowOrHi.textContent = '';
+    if (userGuess === randomNumber) {
+        lastResult.textContent = '正确';
+        lastResult.style.backgroundColor = 'yellow';
+        lowOrHi.textContent = '';
+        setGameOver();
+    }
+    else if (guessCount === 10) {
+        lastResult.textContent = '游戏结束';
+        lowOrHi.textContent = '';
+        setGameOver();
+    }
+
+    else {
+        lastResult.textContent = '错误';
+        lastResult.style.backgroundColor = 'red';
+
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = '低了';
+        }
+        else if (userGuess > randomNumber) {
+            lowOrHi.textContent = '高了';
+        }
+    }
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
 
 }
 
-guessSubmit.addEventListener('click',checkGuess);
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver(){
+    //禁用文本框
+    guessField.disabled=true;
+    //禁用确定键
+    guessSubmit.disabled=true;
+    //创建button元素 button是HTML上的按钮的标签名
+    resetButton=document.createElement('button');
+    //将新生成的元素resetButton设置文本内容
+    resetButton.textContent='开始新的游戏';
+    //将resetButton作为body的小孩加入页面
+    document.body.appendChild(resetButton);
+    //为resetButton设置单机事件侦听器
+    resetButton.addEventListener('click',resetGame);
+
+    //var resetButtonP=document.querySelector("div .resultParas p:last-child");
+    //console.log(resetButtonP);
+    //resetButtonP.style.display="block";
+}
+function resetGame(){
+    guessCount = 1;
+  var resetParas = document.querySelectorAll('.resultParas p');
+  for(var i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+  resetButton.parentNode.removeChild(resetButton);
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+  lastResult.style.backgroundColor = 'white';
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
 
